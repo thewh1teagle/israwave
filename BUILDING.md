3f03ea14e2be3d28f32bcc44360fd7758a120be6
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-09-12 02:30:03 +0300

    add ci

---
+++ BUILDING.md
@@ -0,0 +1,22 @@
+# Building
+
+## Build
+
+```console
+python -m pip install -U build
+python -m build
+```
+
+## Publish
+
+_Get token from https://pypi.org/manage/account/token/ _
+
+```console
+python -m pip install -U twine
+
+python -m build
+python -m twine upload dist/*
+```
+
+_Add `--repository testpypi` to upload to test repository_
+
--- examples/play.py
+++ examples/play.py
@@ -3,7 +3,8 @@ wget https://github.com/thewh1teagle/optispeech/releases/download/v0.1.0/epoch-1
 wget https://github.com/thewh1teagle/optispeech/releases/download/v0.1.0/espeak-ng-data.7z
 wget https://github.com/thewh1teagle/nakdimon-ort/releases/download/v0.1.0/nakdimon.onnx
 
-python3 play.py israwave.onnx espeak-ng-data nakdimon.onnx "שלום! מה קורה?"
+
+python3 play.py israwave.onnx espeak-ng-data nakdimon.onnx 'צַהַ"ל הִתִּיר לְפַרְסֵם אַחַר הַצָּהֳרַיִם (יוֹם ד) כִּי סָמָ"ר גֵּרִי גִּדְעוֹן הנגהאל, בֶּן 24 מִנּוֹף הַגָּלִיל, לוֹחֵם בִּגְדוּד נַחְשׁוֹן (90) שֶׁבַּחֲטִיבַת כְּפִיר, נֶהֱרַג בְּפִגּוּעַ הַדְּרִיסָה הַבֹּקֶר סָמוּךְ לְצֹמֶת אָסָף לְיַד הַיִּשּׁוּב בֵּית אֵל שֶׁבְּבִנְיָמִין'
 """
 
 from israwave import IsraWave
@@ -16,6 +17,6 @@ if __name__ == '__main__':
     
     speech_model = IsraWave(speech_model_path, espeak_data_path)
     niqqud_model = Nakdimon(niqqud_model_path)
-    text = niqqud_model.compute(text)
+    # text = niqqud_model.compute(text)
     waveform = speech_model.create(text)
     waveform.play()
