da2c138fb24740dbb3c36de76ae93407274c699c
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-09-12 12:50:58 +0300

    misc

--- examples/play.py
+++ examples/play.py
@@ -5,7 +5,7 @@ wget https://github.com/thewh1teagle/optispeech/releases/download/v0.1.0/espeak-
 wget https://github.com/thewh1teagle/nakdimon-ort/releases/download/v0.1.0/nakdimon.onnx
 
 
-python3 play.py israwave.onnx espeak-ng-data nakdimon.onnx 'צַהַ"ל הִתִּיר לְפַרְסֵם אַחַר הַצָּהֳרַיִם (יוֹם ד) כִּי סָמָ"ר גֵּרִי גִּדְעוֹן הנגהאל, בֶּן 24 מִנּוֹף הַגָּלִיל, לוֹחֵם בִּגְדוּד נַחְשׁוֹן (90) שֶׁבַּחֲטִיבַת כְּפִיר, נֶהֱרַג בְּפִגּוּעַ הַדְּרִיסָה הַבֹּקֶר סָמוּךְ לְצֹמֶת אָסָף לְיַד הַיִּשּׁוּב בֵּית אֵל שֶׁבְּבִנְיָמִין'
+python3 play.py israwave.onnx espeak-ng-data nakdimon.onnx 'תגידו, גנבו לכם פעם את האוטו ופשוט ידעתם שאין טעם להגיש תלונה במשטרה?'
 """
 
 from israwave import IsraWave
@@ -18,6 +18,6 @@ if __name__ == '__main__':
     
     speech_model = IsraWave(speech_model_path, espeak_data_path)
     niqqud_model = Nakdimon(niqqud_model_path)
-    # text = niqqud_model.compute(text)
+    text = niqqud_model.compute(text)
     waveform = speech_model.create(text)
     waveform.play()
